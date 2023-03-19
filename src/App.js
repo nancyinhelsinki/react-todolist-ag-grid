538440f3df99a7fcfd6ff6127e1521f04e31307b
Ngan Nguyen <huynhngan281195@gmail.com>
2023-03-19 17:25:30 +0200

    to do list with ag-grid

--- src/App.js
+++ src/App.js
@@ -1,23 +1,97 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef } from "react";
+import { AgGridReact } from "ag-grid-react";
+import "ag-grid-community/styles/ag-grid.css";
+import "ag-grid-community/styles/ag-theme-material.css";
 
 function App() {
+  const [todo, setTodo] = useState({ description: "", date: "", priority: "" });
+  const [todos, setTodos] = useState([]);
+  const gridRef = useRef();
+  const [columnDefs] = useState([
+    { field: "description", sortable: true, filter: true },
+    { field: "date", sortable: true, filter: true },
+    {
+      field: "priority",
+      sortable: true,
+      comparator: (valueA, valueB, nodeA, nodeB, isDescending) => {
+        const valueALowercased = valueA.toLowerCase();
+        const valueBLowercased = valueB.toLowerCase();
+
+        if (valueALowercased === valueBLowercased) return 0;
+        if (valueALowercased === "low") return -1;
+        if (valueALowercased === "high") return 1;
+        if (valueALowercased === "medium" && valueBLowercased === "low")
+          return 1;
+        if (valueALowercased === "medium" && valueBLowercased === "high")
+          return -1;
+      },
+      filter: true,
+      cellStyle: (params) =>
+        params.value.toLowerCase() === "high"
+          ? { color: "red" }
+          : { color: "black" },
+    },
+  ]);
+
+  const inputChanged = (event) => {
+    setTodo({ ...todo, [event.target.name]: event.target.value });
+  };
+
+  const addTodo = (event) => {
+    setTodos([...todos, todo]);
+  };
+  const deleteTodo = () => {
+    if (gridRef.current.getSelectedNodes().length > 0) {
+      setTodos(
+        todos.filter(
+          (todo, index) =>
+            index.toString() !== gridRef.current.getSelectedNodes()[0].id
+        )
+      );
+    } else {
+      alert("Select row first");
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div style={{ width: 510, margin: "auto" }}>
+        <input
+          type="text"
+          onChange={inputChanged}
+          placeholder="Description"
+          name="description"
+          value={todo.description}
+        />
+        <input
+          type="date"
+          onChange={inputChanged}
+          placeholder="Date"
+          name="date"
+          value={todo.date}
+        />
+        <input
+          type="text"
+          onChange={inputChanged}
+          placeholder="Priority"
+          name="priority"
+          value={todo.priority}
+        />
+        <button onClick={addTodo}>Add</button>
+        <button onClick={deleteTodo}>Delete</button>
+      </div>
+      <div
+        className="ag-theme-material"
+        style={{ height: 400, width: 600, margin: "auto" }}
+      >
+        <AgGridReact
+          ref={gridRef}
+          onGridReady={(params) => (gridRef.current = params.api)}
+          rowSelection="single"
+          rowData={todos}
+          columnDefs={columnDefs}
+        ></AgGridReact>
+      </div>
     </div>
   );
 }
