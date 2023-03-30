340a4c0e4e3fd365059b037bf2d6a80b0235eb60
Ngan Nguyen <huynhngan281195@gmail.com>
2023-03-30 17:23:44 +0300

    add row animation and floating filter

--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
 import "./App.css";
 import ToDoList from "./components/ToDoList";
-import AppBar from "@mui/material/AppBar";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
 
 function App() {
   return (
     <div className="App">
-      <AppBar>
-        <Toolbar>
-          <Typography variant="h5">My ToDoList</Typography>
-        </Toolbar>
-      </AppBar>
-
       <ToDoList />
     </div>
   );
--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -2,12 +2,6 @@ import React, { useState, useRef } from "react";
 import { AgGridReact } from "ag-grid-react";
 import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-material.css";
-import Button from "@mui/material/Button";
-import TextField from "@mui/material/TextField";
-import Stack from "@mui/material/Stack";
-import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
-import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 
 function ToDoList() {
   const [todoInput, setTodoInput] = useState({
@@ -18,11 +12,22 @@ function ToDoList() {
   const [todoList, setTodoList] = useState([]);
   const gridRef = useRef();
   const [columnDefs] = useState([
-    { field: "description", sortable: true, filter: true },
-    { field: "date", sortable: true, filter: true },
+    {
+      field: "description",
+      sortable: true,
+      filter: true,
+      floatingFilter: true,
+    },
+    {
+      field: "date",
+      sortable: true,
+      filter: true,
+      floatingFilter: true,
+    },
     {
       field: "priority",
       sortable: true,
+      floatingFilter: true,
       comparator: (valueA, valueB) => {
         const valueALowercased = valueA.toLowerCase();
         const valueBLowercased = valueB.toLowerCase();
@@ -48,10 +53,6 @@ function ToDoList() {
     setTodoInput({ ...todoInput, [event.target.name]: event.target.value });
   };
 
-  const changeDate = (newValue) => {
-    setTodoInput({ ...todoInput, date: newValue });
-  };
-
   const addTodo = (event) => {
     setTodoList([...todoList, todoInput]);
     setTodoInput({
@@ -75,38 +76,29 @@ function ToDoList() {
 
   return (
     <div>
-      <Stack
-        direction="row"
-        justifyContent="center"
-        alignItems="center"
-        spacing={2}
-        marginTop="70px"
-      >
-        <TextField
-          onChange={inputChanged}
-          label="Description"
-          variant="standard"
-          name="description"
-          value={todoInput.description}
-        />
-
-        <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="fi">
-          <DatePicker value={todoInput.date} onChange={changeDate} />
-        </LocalizationProvider>
-        <TextField
-          onChange={inputChanged}
-          label="Priority"
-          variant="standard"
-          name="priority"
-          value={todoInput.priority}
-        />
-        <Button variant="contained" onClick={addTodo}>
-          Add
-        </Button>
-        <Button variant="contained" color="error" onClick={deleteTodo}>
-          Delete
-        </Button>
-      </Stack>
+      <input
+        onChange={inputChanged}
+        type="text"
+        placeholder="Description"
+        name="description"
+        value={todoInput.description}
+      />
+      <input
+        onChange={inputChanged}
+        type="date"
+        placeholder="Date"
+        name="date"
+        value={todoInput.date}
+      />
+      <input
+        onChange={inputChanged}
+        type="text"
+        placeholder="Priority"
+        name="priority"
+        value={todoInput.priority}
+      />
+      <button onClick={addTodo}>Add</button>
+      <button onClick={deleteTodo}>Delete</button>
       <div
         className="ag-theme-material"
         style={{ height: 400, width: 600, margin: "auto" }}
@@ -117,6 +109,7 @@ function ToDoList() {
           rowSelection="single"
           rowData={todoList}
           columnDefs={columnDefs}
+          animateRows={true}
         ></AgGridReact>
       </div>
     </div>
